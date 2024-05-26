e8b1e0fb9da59d0cd700ec7922c67226eeda6b90
miguel <vackap@gmail.com>
2024-05-26 04:03:16 -0300

    refactor script

--- scripts/AnimatedSprite2D.gd
+++ scripts/AnimatedSprite2D.gd
@@ -21,18 +21,17 @@ func _input(event):
 
 func set_animation_by_angle(angle: float):
 	
-	
 	if abs(angle) < PI / 2:
 
 		self.flip_h = false
 
-		if angle < -PI/3:
+		if angle < - PI / 3:
 			self.animation = "back"
-		elif angle < -PI/6:
+		elif angle < - PI / 6:
 			self.animation = "up_diagonal"
-		elif angle < PI/6:
+		elif angle < PI / 6:
 			self.animation = "running"
-		elif angle < PI/3:
+		elif angle < PI / 3:
 			self.animation = "diagonal"
 		else:
 			self.animation = "front"
@@ -45,36 +44,16 @@ func set_animation_by_angle(angle: float):
 			
 		self.flip_h = true
 
-		if angle < -PI/3:
+		if angle < - PI / 3:
 			self.animation = "back"
-		elif angle < -PI/6:
+		elif angle < - PI / 6:
 			self.animation = "up_diagonal"
-		elif angle < PI/6:
+		elif angle < PI / 6:
 			self.animation = "running"
-		elif angle < PI/3:
+		elif angle < PI / 3:
 			self.animation = "diagonal"
 		else:
 			self.animation = "front"
-
-	#
-	#if -PI/6  < angle and angle < PI/6:
-		#self.animation = "running"
-		#self.flip_h = false
-	#elif PI/6 < angle and angle < 2*PI/6:
-		#self.animation = "diagonal"
-		#self.flip_h = false
-	#elif 2*PI/6 < angle and angle < 2*PI/3:
-		#self.animation = "front"
-		#self.flip_h = false
-	#elif 2*PI/3 < angle and angle < 5*PI/6:
-		#self.animation = "diagonal"
-		#self.flip_h = true
-	#elif 5*PI/6 < angle and angle < 7*PI/6:
-		#self.animation = "running"
-		#self.flip_h = true
-	#elif 7*PI/6 < angle and angle < 8*PI/6:
-		#self.animation = "up_diagonal"
-		#self.flip_h = true
 
 func _physics_process(delta):
 	
@@ -90,47 +69,9 @@ func _physics_process(delta):
 	translate(direction * SPEED)
 	
 	if direction != Vector2.ZERO:
-		facing_direction = facing_direction.slerp(direction, 0.1)
+		facing_direction = facing_direction.slerp(direction, 0.2)
 
-	#print(facing_direction, "\tangle:", facing_direction.angle(), "\t", "lenght:", facing_direction.length())
-	
 	set_animation_by_angle(facing_direction.angle())
-	# if direction.x > 0:
-		
-	# 	if direction.y == 0:
-			
-	# 		self.animation = "running"
-	# 		self.flip_h = false
-
-	# 	elif direction.y > 0:
-	# 		self.animation = "diagonal"
-	# 		self.flip_h = false
-	# 	else:
-	# 		self.animation = "up_diagonal"
-	# 		self.flip_h = false
-	# elif direction.x < 0:
-		
-	# 	if direction.y == 0:
-			
-	# 		self.animation = "running"
-	# 		self.flip_h = true
-
-	# 	elif direction.y > 0:
-	# 		self.animation = "diagonal"
-	# 		self.flip_h = true
-	# 	else:
-	# 		self.animation = "up_diagonal"
-	# 		self.flip_h = true
-	# elif direction == Vector2.UP:
-
-	# elif direction == Vector2.DOWN:
-	# 	if last_vertical_direction == Vector2.UP:
-	# 		self.animation = "running"
-
-	# 		check_turn_frames_vertical(Vector2.DOWN)
-	# 	else:
-	# 		self.animation = "front"
-	# 		last_vertical_direction = Vector2.DOWN
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):

ef76c4e238643c5aefa9b3ab2111c961162858e0
miguel <vackap@gmail.com>
2024-05-31 11:15:36 -0300

    backdrop + scale script

--- scripts/AnimatedSprite2D.gd
+++ scripts/AnimatedSprite2D.gd
@@ -3,16 +3,21 @@ extends AnimatedSprite2D
 var direction = Vector2.ZERO
 var facing_direction = Vector2.ZERO
 
+var visible_rect: Rect2 = Rect2(0,0,0,0)
+
 var turn_frames: int = 0
 @export var MAX_TURN_FRAMES: int = 8
 @export var ANIMATION_WALK_SPEED: float = 2.0
 @export var ANIMATION_RUN_SPEED: float = 5.0
 
-var SPEED: float = 2.0
+var SPEED: float = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.speed_scale = ANIMATION_WALK_SPEED
+	var viewport = self.get_viewport();
+	visible_rect = viewport.get_visible_rect()
+
 
 func _input(event):
 				#
@@ -54,12 +59,21 @@ func _physics_process(delta):
 		SPEED = 2.0
 		self.speed_scale = ANIMATION_WALK_SPEED
 	
-	translate(direction * SPEED)
+	if direction == Vector2.UP or direction == Vector2.DOWN:	
+		translate(direction * SPEED/3)
+	else:
+		translate(direction * SPEED)
 	
 	if direction != Vector2.ZERO:
 		facing_direction = facing_direction.slerp(direction, 0.15)
 
 	set_animation_by_angle(facing_direction.angle())
+	
+	var new_scale = remap(self.position.y,0, visible_rect.size.y, 0, 1.1)
+	self.scale = Vector2(new_scale, new_scale)
+	
+	
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
